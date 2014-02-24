a7325d5a3fb808d2fed92748937dc27b4f6dabbe
Spencer Rogers <spencer1248@gmail.com>
2014-02-24 16:30:22 -0500

    Update wolf tests to adapt to AMS

--- spec/requests/api/wolves_spec.rb
+++ spec/requests/api/wolves_spec.rb
@@ -22,8 +22,8 @@ describe 'API' do
 
       response_hash = JSON.parse(response.body)
 
-      expect(response_hash['page']).to eq 2
-      expect(response_hash['per_page']).to eq 5
+      expect(response_hash['meta']['page']).to eq 2
+      expect(response_hash['meta']['per_page']).to eq 5
     end
 
     it 'generates a correctly structured JSON response' do
@@ -32,8 +32,10 @@ describe 'API' do
       get api_wolves_path(page: 1, per_page: 3)
 
       expect(JSON.parse(response.body)).to eq({
-        'page' => 1,
-        'per_page' => 3,
+        'meta' => {
+          'page' => 1,
+          'per_page' => 3,
+        },
         'wolves' => wolves.slice(0,3).map do |wolf|
         {
           'name' => wolf.name,
